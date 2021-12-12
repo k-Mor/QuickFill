47f86e2aa318fc68d6fa4a7c8e71c63b6802f513
k-Mor <22819877+k-Mor@users.noreply.github.com>
2021-12-11 18:16:16 -0800

    Update QuickFill.ps1

--- QuickFill.ps1
+++ QuickFill.ps1
@@ -1,13 +1,15 @@
-﻿<#
-    Author: Kaleb Morneo
-    Version: 12/10/21
-    
-    .DESCRIPTION
-        The purpose of this script is to.. 
-#>
+# Author: Kaleb Moreno
+# Version: 12/10/21 
+
+# .DESCRIPTION
+    # The purpose of this script is to.. 
+        
+
+
 
 
 function main() {
+
     
     # General vars
     $terminate = -1
@@ -15,7 +17,9 @@ function main() {
     
     # First set the location
     # TODO : Configure the relative path 
-    Set-Location 'C:\Users\MorenKS\myStuff\psScripts'
+    # Set-Location 'C:\Users\MorenKS\myStuff\psScripts'
+    Set-Location "C:\myStuff"
+
 
     # Get the JSON file
     $jsonFileObject = Get-Content '.\responses.json' | Out-String | ConvertFrom-Json
@@ -24,26 +28,24 @@ function main() {
     $fileResponse = Read-Host "Would you like to use a custom response file?"
     Write-Output "`n"
 
+    # Grabbing the main menu
     $mainMenu = $jsonFileObject.mainMenu
 
     # Show the main menu
-    showMenu($mainMenu, $null)
+    showMenu $mainMenu
 
     while($userResponse -ne $terminate) { 
         $userResponse = Read-Host "Enter a sub option"
-         Write-Output "This is what you said: $($userResponse)"
         
         # Cast the var to an int 
-        # $userResponse = [int]$userResponse
+        $userResponse = [int]$userResponse
 
-        showMenu($mainMenu, $userResponse)
+        showMenu $mainMenu $userResponse
     }
 }
 
 
 function showMenu($theJson, $theRes) {
-
-    #Write-Output "This is the Response: $($theRes)"
     
     # The menu header
     $header = $theJson.name
@@ -67,11 +69,17 @@ function showMenu($theJson, $theRes) {
         if ($options.name[$i] -like "*xit*") {
             Write-Output "-1 $($options.name[$i])"
         } else {
-
-            if ($theRes -eq $i) { Write-Output $options.name }
             Write-Output "$($i) $($options.name[$i])"
         }
     }
+
+    # If the input sub menu option is found
+    if ($theRes -ne $null) {
+        Write-Output "TRUE $($theRes)"  
+        showMenu $options[$theRes]
+    }
+
+    $theRes = $null
 }
 
 # TODO
