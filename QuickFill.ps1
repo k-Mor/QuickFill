3b702295f26e41e57a5073b57019050d77686db6
k-Mor <22819877+k-Mor@users.noreply.github.com>
2021-12-16 16:56:24 -0800

    Just some cleaning and refactoring

--- QuickFill.ps1
+++ QuickFill.ps1
@@ -29,196 +29,209 @@
 # TODO make the unicode easier to change / replace
 function main() {
 
-    # Create the enviornment to run scripts
-    # Set-ExecutionPolicy -ExecutionPolicy Bypass
-    
-    # General vars
-    $terminate = -1
-    $mainMenu = $jsonFileObject.mainMenu
-    
-    # First set the location
-    # TODO : Configure the relative path 
-    # Set-Location 'C:\Users\MorenKS\myStuff\psScripts'
-    Set-Location "C:\myStuff" #laptop
-    # Set-Location "C:\Users\Kaleb\OneDrive\Documents"
+  # Create the enviornment to run scripts
+  # Set-ExecutionPolicy -ExecutionPolicy Bypass
+  
+  # General vars
+  $terminate = -1
+  $mainMenu = $jsonFileObject.mainMenu
+  
+  # First set the location
+  # TODO : Configure the relative path 
+  # Set-Location 'C:\Users\MorenKS\myStuff\psScripts'
+  # Set-Location "C:\myStuff" #laptop
+  Set-Location "C:\Users\Kaleb\OneDrive\Documents"
 
 
-    # Get the JSON file
-    $jsonFileObject = Get-Content '.\newResponses.json' | Out-String | ConvertFrom-Json
+  # Get the JSON file
+  $jsonFileObject = Get-Content '.\newResponses.json' | Out-String | ConvertFrom-Json
 
-    # Prompt for response file type
-    $fileResponse = formatPrompt "Would you like to use a custom response file?"
-    Clear-Host
+  # Prompt for response file type
+  $fileResponse = formatPrompt "Would you like to use a custom response file?"
+  Clear-Host
 
-    # Grabbing the main menu
-    $mainMenu = $jsonFileObject.mainMenu
+  # Grabbing the main menu
+  $mainMenu = $jsonFileObject.mainMenu
 
-    # Show the main menu
-    showMenu $mainMenu
+  # Show the main menu
+  showMenu $mainMenu
 
-    while ($userResponse -ne $terminate) { 
-        $userResponse = formatPrompt "Enter a sub option"
-        
-        # Get the sub menu options
-        getSubMenu $mainMenu $userResponse
+  while ($userResponse -ne $terminate) { 
+      $userResponse = formatPrompt "Enter a sub option"
+      
+      # Breaking the loop if the input is -1
+      if ($userResponse -like "*$($terminate)*") { break }
+      
+      # Get the sub menu options
+      getSubMenu $mainMenu $userResponse
 
-        # Get an option from the sub menu
-        formatResponses $jsonFileObject $mainMenu $userResponse
+      # Get an option from the sub menu
+      formatResponses $jsonFileObject $mainMenu $userResponse
 
-        # Clear the questions
-        Clear-Host
+      # Clear the questions
+      Clear-Host
 
-        # Redirect back to the main menu
-        showMenu $mainMenu
-    }
+      # Redirect back to the main menu
+      showMenu $mainMenu
+  }
 }
 
 
 function showMenu($theJson) {
-    
-    # The menu header
-    $header = $theJson.name
+  
+  # The menu header
+  $header = $theJson.name
 
-    # The primary options
-    $options = $theJson.options 
+  # The primary options
+  $options = $theJson.options 
 
-    # Dashes for the menu header
-    $dashes = "-" * $header.Length
-    
-    # Writing the menu header
-    Write-Host $dashes
-    Write-Host $header
-    Write-Host $dashes
-    # Write-Host "`n"
-     
-    # The options for the main menu
-    for ($i = 0; $i -lt $options.name.Length; $i++) {
+  # Dashes for the menu header
+  $dashes = "-" * $header.Length
+  
+  # Writing the menu header with bolded text
+  Write-Host $dashes -ForegroundColor White
+  Write-Host $header -ForegroundColor White
+  Write-Host $dashes -ForegroundColor White
+  # Write-Host "`n"
+   
+  # The options for the main menu
+  for ($i = 0; $i -lt $options.name.Length; $i++) {
 
-        # Changing the number designation from last indicie to -1
-        if ($options.name[$i] -like "*xit*") {
-            Write-Host "(-1) $($options.name[$i])"
-        }
-        else {
-            Write-Host "($($i)) $($options.name[$i])"
-        }
-    }
+      # Changing the number designation from last indicie to -1
+      if ($options.name[$i] -like "*xit*") {
+          Write-Host "(-1) $($options.name[$i])"
+      }
+      else {
+          Write-Host "($($i)) $($options.name[$i])"
+      }
+  }
 }
 
 
 function getSubMenu($theMainMenu, $theRes) { 
 
-    Clear-Host
-    # If the input sub menu option is found
-    if ($null -ne $theRes) {
-        showMenu $theMainMenu.options[$theRes]
-    }
+  # Clearing previous menus when moving between them
+  Clear-Host
+
+  # If the input sub menu option is found
+  if ($null -ne $theRes) {
+      showMenu $theMainMenu.options[$theRes]
+  }
 }
 
 
 
 function formatResponses($theJSONFile, $theMainMenu, $theSubRes) {
-    $terminate = -1
-    if ($theSubRes -ne $terminate) {
-        
-        # The values that must be replaced in the JSON file
-        $phone = $theJSONFile.config.phone
-        $hours = $theJSONFile.config.hours
-        $APLink = $theJSONFile.config.APlink
-        $days = $theJSONFile.config.days
-        
-    
-        # Grabbing an additional response for indexing
-        $outputSelection = formatPrompt "Select an option"
-        $outputSelection = [int]$outputSelection
-        
-        # Grabbing the links 
-        $links = $theMainMenu.options[$theSubRes].options[$outputSelection].links
-        
-        # Grabbing more data
-        $txt = $theMainMenu.options[$theSubRes].options[$outputSelection].text
-        $questions = $theMainMenu.options[$theSubRes].options[$outputSelection].questions
+  $terminate = -1
+  $empty = 0
+  $minLen = 1
+  $options = $theMainMenu.options[$theSubRes].options
 
-        # If user just wants resources skip all this
-        if ($theSubRes -ne 0 -And $txt.Length -ge 1) { 
-            $fname = formatPrompt "Enter the first name of the caller"
-            $moreInfo = formatPrompt "Do you need more data in the response? ex.[Room #], [Service Name]"
-    
-            # Checking to see if the responses need more information
-            if ($moreInfo -like "*y*") {
-                $room = formatPrompt "Enter the room number"
-                $service = formatPrompt "Enter the disrupted service"
-            }
-    
+  # Grabbing an additional response for indexing
+  $outputSelection = formatPrompt "Select an option"
+  
+  # This condition is checking for two things - the termination character
+  # And if the last menu item is selected. This is the reserved spot for sub menu termination
+  if ($theSubRes -ne $terminate -And $options[$outputSelection] -ne $options[$terminate]) {
+
+      # The values that must be replaced in the JSON file
+      $phone = $theJSONFile.config.phone
+      $hours = $theJSONFile.config.hours
+      $APLink = $theJSONFile.config.APlink
+      $days = $theJSONFile.config.days
+          
+      # Grabbing the links 
+      $links = $options[$outputSelection].links
+      
+      # Grabbing more data
+      $txt = $options[$outputSelection].text
+      $questions = $options[$outputSelection].questions
 
 
-            # Replacing the values
-            $txt = $txt `
-                -replace "{time}", "$(formatTime)" `
-                -replace "{fname}", $fname `
-                -replace "{phone}", "$($phone)" `
-                -replace "{hours}", "$($hours)" `
-                -replace "{days}", "$($days)" `
-                -replace "{APlink}", "$($APlink)" `
-                -replace "{service}", "$($service)" `
-                -replace "{room}", "$($room)"
-    
-            
-            # User feedback
-            if ($txt.Length -ge 1 -And $null -ne $txt) {
-                Set-Clipboard -value $txt
-                feedback
-            }
-        }
 
-        # If there are questions
-        # Clear-Host
-        $questions
+      # If user just wants resources skip all this
+      if ($theSubRes -ne $empty -And $txt.Length -ge $minLen) { 
+          $fname = formatPrompt "Enter the first name of the user"
 
-        # Allow for more extensive documentation options
-        foreach ($link in $links) {
-            openLink($link)
-        }
-        
-    }
+          #Write-Host "This is the thing: $($options[$outputSelection].name)"
+
+          # Asking additional questions if that is indicated in the response name
+          if ($options[$outputSelection].name -Like "*~*") { 
+            $moreInfo = formatPrompt "Enter the additional data"
+          }
+
+          # Replacing the values
+          $txt = $txt `
+              -replace "{time}", "$(formatTime)" `
+              -replace "{fname}", $fname `
+              -replace "{phone}", "$($phone)" `
+              -replace "{hours}", "$($hours)" `
+              -replace "{days}", "$($days)" `
+              -replace "{APlink}", "$($APlink)" `
+              -replace "{data}", "$($moreInfo)"
+  
+          
+          # User feedback
+          if ($txt.Length -ge $minLen -And $null -ne $txt) {
+              Set-Clipboard -value $txt
+              feedback
+          }
+      }
+
+      # Handling the questions
+      Clear-Host
+      Write-Host "[$($options[$outputSelection].name)]"
+      Write-Host $questions
+
+      # Handling the items with no links 
+      if ($null -eq $links) {
+        formatPrompt("Continue?")
+      }
+
+      # Allow for more extensive documentation options
+      foreach ($link in $links) {
+          openLink($link)
+      }
+      
+  }
 }
 
 function feedBack() {
-    $delayTime = 1.4
-    Write-Host "`n** Ticket response was copied to your clipboard **`n" -ForegroundColor DarkGreen
-    Start-Sleep -Seconds $delayTime
-    Clear-Host
+  $delayTime = 1.4
+  Write-Host "`n** Ticket response was copied to your clipboard **`n" -ForegroundColor DarkGreen
+  Start-Sleep -Seconds $delayTime
 }
 
 function formatPrompt($thePrompt) {
-    return "$(Write-Host "$($thePrompt): " -ForegroundColor Red -NoNewline) $(Read-Host)"
+  return "$(Write-Host "$($thePrompt): " -ForegroundColor Red -NoNewline) $(Read-Host)"
 }
 
 
 function formatTime() {
-    $currentTime = Get-Date -Format HH
-    $returnVal = $null
-    $noon = 12
+  $currentTime = Get-Date -Format HH
+  $returnVal = $null
+  $noon = 12
 
-    if ($currentTime -lt $noon) {
-        $returnVal = "morning"
-    }
-    else {
-        $returnVal = "afternoon"
-    }
-    return $returnVal
+  if ($currentTime -lt $noon) {
+      $returnVal = "morning"
+  }
+  else {
+      $returnVal = "afternoon"
+  }
+  return $returnVal
 }
 
 
 function openLink($theLink) {
-    $delayTime = 1.4
+  $delayTime = 1.4
 
-    # Handling the documentation links
-    $openLinks = formatPrompt "`nDo you need this resources? ($($theLink.label))"
-    if ($openLinks -like "*y*") {
-        Write-Host "** Opening: $($theLink.label) **`n" -ForegroundColor DarkGreen
-        Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ArgumentList $theLink.uri
-        Start-Sleep $delayTime
-    }
+  # Handling the documentation links
+  $openLinks = formatPrompt "Do you need this resources? ($($theLink.label))"
+  if ($openLinks -like "*y*") {
+      Write-Host "** Opening: $($theLink.label) **`n" -ForegroundColor DarkGreen
+      Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ArgumentList $theLink.uri
+      Start-Sleep $delayTime
+  }
 }
 
 
